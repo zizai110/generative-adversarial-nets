ccde23fd5f7443c0ff1bdfbb1e5e866b2ff65f45
gdouzas <gdouzas@icloud.com>
2017-03-06 10:55:08 +0200

    Adde docstring for GAN class

--- ganetwork.py
+++ ganetwork.py
@@ -79,33 +79,7 @@ def mini_batch_indices_generator(n_samples, batch_size):
 
 
 class BaseGAN:
-
-    """Base class for GANs and CGANs.  
-    
-    Parameters
-    ----------
-    discriminator_layers : list of (int, activation function) tuples
-        Each tuple represents the number of neurons and the activation 
-        function of the discriminator's corresponding layer. The 
-        number of neurons and the activation function for the first 
-        layer should be equal to the number of features of the X 
-        matrix and equal to None, correspondingly i.e. (X.shape[1], None). 
-        The number of neurons and the activation function for the last 
-        layer should be equal to 1 and equal to None, correspondingly i.e. 
-        (1, None).
-    generator_layers : list of (int, activation function) tuples
-        Each tuple represents the number of neurons and the activation 
-        function of the generators's corresponding layer. The 
-        number of neurons and the activation function for the first 
-        layer should be equal to the number of features of the Z 
-        matrix and equal to None, correspondingly i.e. (Z.shape[1], None). 
-        The number of neurons and the activation function for the last 
-        layer should be equal to the number of features of the X matrix 
-         and equal to None, correspondingly i.e. (X.shape[1], None).
-    discriminator_optimizer: TensorFlow optimizer
-        The number of CPUs to use to do the computation. -1 means
-        'all CPUs'.
-    """
+    """Base class for GANs and CGANs."""  
 
     def __init__(self, discriminator_layers, generator_layers, discriminator_optimizer=OPTIMIZER, generator_optimizer=OPTIMIZER):
         self.discriminator_layers = discriminator_layers
@@ -159,6 +133,32 @@ class BaseGAN:
 
 
 class GAN(BaseGAN):
+    """
+    Parameters
+    ----------
+    discriminator_layers : list of (int, activation function) tuples
+        Each tuple represents the number of neurons and the activation 
+        function of the discriminator's corresponding layer. The 
+        number of neurons and the activation function for the first 
+        layer should be equal to the number of features of the X 
+        matrix and equal to None, correspondingly i.e. (X.shape[1], None). 
+        The number of neurons and the activation function for the last 
+        layer should be equal to 1 and equal to None, correspondingly i.e. 
+        (1, None).
+    generator_layers : list of (int, activation function) tuples
+        Each tuple represents the number of neurons and the activation 
+        function of the generators's corresponding layer. The 
+        number of neurons and the activation function for the first 
+        layer should be equal to the number of features of the Z 
+        matrix and equal to None, correspondingly i.e. (Z.shape[1], None). 
+        The number of neurons and the activation function for the last 
+        layer should be equal to the number of features of the X matrix 
+         and equal to None, correspondingly i.e. (X.shape[1], None).
+    discriminator_optimizer: TensorFlow optimizer
+        The optimizer for the discriminator.
+    generator_optimizer: TensorFlow optimizer
+        The optimizer for the generator.
+    """
 
     def train(self, X, nb_epoch, batch_size, discriminator_steps=1, verbose=1):
         super()._initialize_training_parameters(X, None, batch_size)
